fe92daeff55b40e2b66165d2e29468378074a422
Botmduser <Botdeploy698@gmail.com>
2025-08-08 15:55:06 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,49 @@
+const { cmd } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const config = require('../config');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "online", "a"],
+    desc: "Check bot is alive or not",
+    category: "main",
+    react: "📍",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        const status = `
+╭───〔 *🤖 HASHAN-MD STATUS* 〕───◉
+│✨ *HASHAN-MD ALIVE NOW*
+│
+│🧠 *Owner:* Hashiya
+│⚡ *Version:* 1.0.0
+│📝 *Prefix:* [${config.PREFIX}]
+│📳 *Mode:* public
+│💾 *RAM:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+│🖥️ *Host:* ${os.hostname()}
+│⌛ *Uptime:* ${runtime(process.uptime())}
+╰────────────────────◉
+> HASHAN-MD WHATSAPP BOT`;
+
+        await conn.sendMessage(from, {
+            image: { url: config.MENU_IMAGE_URL },
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363395674230271@newsletter',
+                    newsletterName: 'HASHAN-MD',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Alive Error:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
