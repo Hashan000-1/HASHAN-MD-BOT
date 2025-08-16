f1756772779d6dffa6cde524107505572bacada3
Hashan000-1 <pabasarahashan977@gmail.com>
2025-08-16 14:11:39 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *HASHAN-MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *DEATH-NOTE-MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -42,7 +42,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363395674230271@newsletter',
-                    newsletterName: "HASHAN-MD",
+                    newsletterName: "DEATH-NOTE-MD",
                     serverMessageId: 143
                 }
             }
